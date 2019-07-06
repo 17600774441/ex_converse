36cbd2619e699b5669d30388645ced6bc2fbc6f0
17600774441 <17600774441@163.com>
2019-07-06 17:36:27 +0800

    商品列表页完成

---
+++ public/rewrite_object/JS/checkbox.js
@@ -0,0 +1,16 @@
+(function(){
+    // 记住用户名选中
+    document.addEventListener("DOMContentLoaded",function(event){
+      var $checkbox = document.getElementsByClassName("checkbox");
+      function check() {
+        this.classList.toggle("checked");
+      };
+      for (var i = 0; i < $checkbox.length; i++) {
+        $checkbox[i].onclick = check.bind($checkbox[i]);
+      }
+    });
+    
+})();
+/* <div class="checkbox">
+  记住用户名
+</div> */
